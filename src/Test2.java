3a4c37f4ceeb2a6df1f32b106b243bbe7299e7aa
kantishri <kantishri@yahoo.com>
2022-10-07 14:16:49 -0700

    commiting to branch x

---
+++ src/Test2.java
@@ -0,0 +1,15 @@
+
+public class Test2 {
+	private void psvm() {
+		String hungry = "yes";
+		if(hungry == "yes")
+		{
+			System.out.println("eat something....");
+		}
+		else {
+			System.out.println("do work...........");
+
+		}	
+	}
+}
+
